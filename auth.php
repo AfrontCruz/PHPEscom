ead2e406b010edf0bb5a597999595a67724451d3
AfrontCruz <danafront@gmail.com>
2021-04-27 21:13:43 -0500

    27/04/2021

---
+++ auth.php
@@ -0,0 +1,21 @@
+<?php
+    require( './database/database.php');
+    $curp = $_POST['curp'];
+    $clave = $_POST['clave'];
+
+    $db = new database();
+    $db->obtenerConexion();
+    $sql = "CALL sp_autentica('$curp','$clave')";
+    $result = $db->read( $sql );
+    
+    if( $result[0]->respuesta == 1 ){
+        echo "Autenticación exitosa";
+        ?>
+        <script>
+            setTimeout( () => { window.location.replace("/dashboard.php") }, 3000 );
+        </script>
+<?php
+    }
+    else
+        echo "Error: " . $result[0]->mensaje;
+?>
